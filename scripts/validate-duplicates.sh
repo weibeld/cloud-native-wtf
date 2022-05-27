weibeld/cloud-native-wtf

commit fbb004c7c2d40de7910fc2dafac234fc0b74edc4

Adapt schema validation scripts to always validate all files
Validate all files first and then exit with a non-zero exit code if any
of them failed, rather than exiting as soon as the first file fails.
This allows to see all errors in a single pass in case that there are
multiple failing files.

scripts/validate-schema.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # 
-# Validate all YAML data files with the corresponding schemas.
+# Validate all YAML data files with the corresponding schemas. The script
+# exits with exit code 1 if any of the data files is invalid.
 #
 # Requires the following npm packages to be installed:
 #   - ajv-cli [1]
@@ -11,6 +12,10 @@
 # [1] https://www.npmjs.com/package/ajv-cli
 # [2] https://www.npmjs.com/package/ajv-formats
 
+unset failed
+
 for f in projects organisations tags; do
-  ajv --spec draft2020 -c ajv-formats -s schema/"$f".json -d "$f".yaml
+  ajv --spec draft2020 -c ajv-formats -s schema/"$f".json -d "$f".yaml || failed=1
 done
+
+[[ -z "$failed" ]] && exit 0 || exit 1

commit 3a0f6f2ba51b3d5567375e97630b1ebde24d370f

Fix bug in scripts related to while loop in sub-shell
Previously, the 'failed' variable was set in a while loop used as
'files | while; do ... done;'. In this case, the while loop runs in a
sub-shell because it is the right-hand side of a pipe. That means that
any modifications to the 'failed' variable in the while loop were lost
outside of the loop. This caused the scripts to terminate with a zero
exit code even if one of the validations failed.

This was a bug introduced in fbb004c7c2d40de7910fc2dafac234fc0b74edc4.

Now, the 'failed' status is saved in a file rather than in a variable
which guarantees that the status is seen identically from all execution
environments.

scripts/validate-duplicates.sh
@@ -15,7 +15,7 @@
 # [1] https://github.com/mikefarah/yq
 
 . scripts/_commons.sh
-unset failed
+failed=$(mktemp)
 
 # Normalisation of names as per the above definition.
 normalise() {
@@ -23,13 +23,13 @@ normalise() {
 }
 
 files | while read f; do
-  duplicates=$(cat "$f" | yq '.[].name' | normalise | sort | uniq -d) 
+  duplicates=$(cat "$f" | yq '.[].name' | normalise | sort | uniq -d || echo 1 >"$failed") 
   if [[ -z "$duplicates" ]]; then
     echo "$f valid"
   else
     printf "$f invalid\n$(colour_red)Duplicates:\n$duplicates$(colour_reset)\n"
-    failed=1
+    echo 1 >"$failed"
   fi
 done
 
-[[ -z "$failed" ]] && exit 0 || exit 1
+[[ -s "$failed" ]] && exit 1 || exit 0

scripts/validate-schema.sh
@@ -10,10 +10,10 @@
 # [2] https://www.npmjs.com/package/ajv-formats
 
 . scripts/_commons.sh
-unset failed
+failed=$(mktemp)
 
 files | while read f; do
-  ajv --spec draft2020 -c ajv-formats -s schema/"${f%.*}".json -d "$f" || failed=1
+  ajv --spec draft2020 -c ajv-formats -s schema/"${f%.*}".json -d "$f" || echo 1 >"$failed"
 done
 
-[[ -z "$failed" ]] && exit 0 || exit 1
+[[ -s "$failed" ]] && exit 1 || exit 0